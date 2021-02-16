0e5f0a043b74c560040243fc81d4b9c7212652b1
Edward Gomez <2865652+heloman1@users.noreply.github.com>
2021-02-15 20:45:22 -0500

    Add simple cmd arg checking

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,38 @@ import https from "https";
 import http from "http";
 import fs from "fs";
 
-let credentials = {
-    key: fs.readFileSync("ssl/privkey.pem"),
-    cert: fs.readFileSync("ssl/fullchain.pem"),
-};
+const IS_INSECURE = process.argv[2] === "insecure" ? true : false;
+let PORT : number;
+
+if (process.argv[3]) {
+    PORT = +process.argv[3];
+} else if (IS_INSECURE) {
+    PORT = 8080;
+} else {
+    PORT = 443;
+}
 
 let app = express();
 
 app.use(express.static("public"));
 
+let httpServer: https.Server | http.Server;
+if (IS_INSECURE) {
+    console.log("WARNING: NOT using HTTPS");
+    httpServer = http.createServer(app);
+} else {
+    let credentials = {
+        key: fs.readFileSync("ssl/privkey.pem"),
+        cert: fs.readFileSync("ssl/fullchain.pem"),
+    };
 
-//let httpServer = http.createServer(app);
-let httpsServer = https.createServer(credentials, app);
+    httpServer = https.createServer(credentials, app);
+}
 
-httpsServer.listen(443, () => {
-    console.log(`Listening on https://edwardgomez.dev`);
+httpServer.listen(PORT, () => {
+    console.log(
+        `Listening on ${
+            IS_INSECURE ? "http" : "https"
+        }://edwardgomez.dev:${PORT}`
+    );
 });
