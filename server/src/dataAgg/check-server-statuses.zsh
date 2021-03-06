6f05df42b8cf84f46133f481a72a86dc37eb4300
Edward Gomez <2865652+heloman1@users.noreply.github.com>
2021-03-05 21:43:00 -0500

    server now controls what games to search

--- server/src/dataAgg/check-server-statuses.zsh
+++ server/src/dataAgg/check-server-statuses.zsh
@@ -2,9 +2,9 @@
 
 cd ~/
 
-gameList=("minecraft" "terraria" "7dtd" "starbound")
-for game in $gameList ; do
-    for server_folder in $game/* ; do
-            echo \{ \"server\" : \"$server_folder\", \"details_string\" : \"$(./$server_folder/*server details | sed 's/\x1b\[[0-9;]*m//g' | egrep "Status:.\w")\" \} &
+
+for game in $(jq ".[]" "$1"); do
+    for server_folder in $game/*; do
+        echo \{ \"server\" : \"$server_folder\", \"details_string\" : \"$(./$server_folder/*server details | sed 's/\x1b\[[0-9;]*m//g' | egrep "Status:.\w")\" \} &
     done
 done
