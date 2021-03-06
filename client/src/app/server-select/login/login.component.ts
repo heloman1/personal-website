0522d108c258d558cfb76d89f8f25aa4f5973b76
Edward Gomez <2865652+heloman1@users.noreply.github.com>
2021-03-06 03:42:35 -0500

    Simplified Login code

--- client/src/app/server-select/login/login.component.ts
+++ client/src/app/server-select/login/login.component.ts
@@ -13,35 +13,25 @@ export class LoginComponent implements OnInit {
     });
     feedbackMessageCSSClass = 'hidden';
     feedbackMessage = '';
-
     constructor(private firebase_service: LoginService) {}
+
+    emailInputMessage() {
+        if (this.email.hasError('required')) {
+            return 'You must enter a value';
+        }
+        return this.email.hasError('email') ? 'Not a valid email' : '';
+    }
 
     async submit() {
         if (this.email.valid) {
             const email = this.email.value;
             let success = await this.firebase_service.sendSignInEmail(email);
             if (success === true) {
-                this.writeMessage(`A signin email has been sent to ${email}`);
+                this.feedbackMessage = `A signin email has been sent to ${email}`;
             } else {
-                this.writeMessage(
-                    'An error occured when attempting to send the email.'
-                );
+                this.feedbackMessage =
+                    'An error occurred when attempting to send the email.';
             }
-        }
-    }
-
-    getEmailErrorMessage() {
-        if (this.email.hasError('required')) {
-            return 'You must enter a value';
-        }
-        return this.email.hasError('email') ? 'Not a valid email' : '';
-    }
-    writeMessage(msg: string) {
-        this.feedbackMessage = msg;
-        if (this.feedbackMessage == '') {
-            this.feedbackMessageCSSClass = 'hidden';
-        } else {
-            this.feedbackMessageCSSClass = '';
         }
     }
 
