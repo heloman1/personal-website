18539ae1ad0124e88f68b4fe31bf0d225ca22c84
Edward Gomez <2865652+heloman1@users.noreply.github.com>
2021-03-07 22:24:59 -0500

    im not sure

--- server/src/firebase.ts
+++ server/src/firebase.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import * as admin from "firebase-admin";
+import admin from "firebase-admin";
 import config from "./config";
 let conf = config.getConfig();
 let firebaseAdmin = admin.initializeApp({
