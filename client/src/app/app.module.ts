7f171313036b224a7afe75bae2d07349a017aa28
Edward Gomez <2865652+heloman1@users.noreply.github.com>
2021-03-08 15:46:44 -0500

    Remove test component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,7 +11,6 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 import { ServerSelectComponent } from './server-select/server-select.component';
 import { LoginService } from './services/firebase.service';
 import { LoginComponent } from './server-select/login/login.component';
@@ -22,7 +21,6 @@ import { RefreshIconComponent } from './server-select/refresh-icon/refresh-icon.
 @NgModule({
     declarations: [
         AppComponent,
-        TestComponent,
         ServerSelectComponent,
         LoginComponent,
         ServerCardComponent,
