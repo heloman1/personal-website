f8b4514786a6c2e11d4676c352424d711c5325ac
Edward Gomez <2865652+heloman1@users.noreply.github.com>
2021-03-08 16:35:57 -0500

    Missed a test component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TestComponent } from './test/test.component';
 import { LoginComponent } from './server-select/login/login.component';
 import { ServerSelectComponent } from './server-select/server-select.component';
 const routes: Routes = [
-    { path: 'test-path', component: TestComponent },
     { path: 'servers', component: ServerSelectComponent },
     { path: 'servers/login', component: LoginComponent },
 ];
