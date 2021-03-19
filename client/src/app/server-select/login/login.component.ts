281c3486d1ba3987e9c9edb6ad1ce01fdbd88597
Edward Gomez <2865652+heloman1@users.noreply.github.com>
2021-03-19 15:18:57 -0400

    Added fadein to login text

--- client/src/app/server-select/login/login.component.ts
+++ client/src/app/server-select/login/login.component.ts
@@ -1,31 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { LoginService } from 'src/app/services/firebase.service';
-import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
     email = new FormControl('', {
         validators: [Validators.required, Validators.email],
     });
     feedbackMessage = '';
     disableSend = false;
-    constructor(
-        private firebase_service: LoginService,
-        private router: Router
-    ) {}
+    fadeIn = false;
+    constructor(private firebase_service: LoginService) {}
 
-    ngOnInit() {
-        this.firebase_service.tryCompleteSignIn().then(async (success) => {
-            if (success) {
-                this.router.navigateByUrl('/servers'); // "Redirect" away from sign-in url
-            }
-        });
-    }
     emailInputMessage() {
         if (this.email.hasError('required')) {
             return 'You must enter a value';
@@ -44,6 +34,7 @@ export class LoginComponent implements OnInit {
                 this.feedbackMessage =
                     'An error occurred when attempting to send the email.';
             }
+            this.fadeIn = true;
         }
     }
 }
