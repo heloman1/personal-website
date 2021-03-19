2eee17032f6b54d1ca503a5d9402e0985aca1b99
Edward Gomez <2865652+heloman1@users.noreply.github.com>
2021-03-19 19:40:48 -0400

    Minor tweaks

--- client/src/app/server-select/server-select.component.ts
+++ client/src/app/server-select/server-select.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/services/firebase.service';
-import { QueryParams, ServerData } from './types';
+import { QueryParams, ServerData, ServerDataValue } from './types';
 import { Router } from '@angular/router';
 import { TitleService } from '../services/title.service';
 @Component({
@@ -12,7 +12,7 @@ import { TitleService } from '../services/title.service';
 export class ServerSelectComponent implements OnInit, OnDestroy {
     constructor(
         private loginService: LoginService,
-        private http: HttpClient,
+        private _httpClient: HttpClient,
         private router: Router,
         private title: TitleService
     ) {}
@@ -29,7 +29,14 @@ export class ServerSelectComponent implements OnInit, OnDestroy {
             if (user) {
                 this.signedIn = true;
                 // Load card data
-                await this.fetchCardData();
+                this.http.functions.fetchCardData().then((data) => {
+                    this.serverData = data;
+
+                    // serverData is technically be missing queryDone keys
+                    // This happens to add them
+                    // Emphasis on "happens to"
+                    this.disableInvalidCards();
+                });
             } else {
                 this.signedIn = false;
             }
@@ -43,135 +50,37 @@ export class ServerSelectComponent implements OnInit, OnDestroy {
 
     async onCardButtonClick(query: QueryParams) {
         // Disable all buttons
-        this.iterateServerKeys((game, server) => {
-            this.serverData[game][server].queryDone = false;
+        this.iterateServerKeys((data) => {
+            data.queryDone = false;
         });
         // Do the query
-        let statusCode = await this.sendCommand(query);
+        let statusCode = await this.http.functions.sendCommand(query);
         // Update online state
-        this.handleServerResponse(query, statusCode);
+        this.http.handleServerResponse(query, statusCode);
         // Reenable appropriate buttons
         this.disableInvalidCards();
-    }
-
-    async sendCommand(data: QueryParams) {
-        try {
-            let res = await this.http
-                .post('/backend/server-command', null, {
-                    headers: {
-                        Authorization: `Bearer ${await this.loginService.firebase_auth.currentUser!.getIdToken()}`,
-                    },
-                    params: data,
-                    responseType: 'text',
-                    observe: 'response',
-                })
-                .toPromise();
-            return res.status;
-        } catch (error) {
-            if (error.status) {
-                return error.status as number;
-            } else {
-                // There was an actual error
-                throw error;
-            }
-        }
-    }
-
-    handleServerResponse(query: QueryParams, statusCode: number) {
-        const { game, server, command } = query;
-        // Update what's online
-        switch (statusCode) {
-            case 200:
-                switch (command) {
-                    case 'start':
-                    case 'restart':
-                        this.serverData[game][server].is_online = true;
-                        this.statusText = 'Command Successful';
-                        break;
-                    case 'stop':
-                        this.serverData[game][server].is_online = false;
-                        this.statusText = 'Command Successful';
-                        break;
-                    default:
-                        this.statusText =
-                            'Unimplemented Commandm contact the developer';
-                        break;
-                }
-                break;
-            case 400:
-                this.statusText = 'Client Syntax, contact the developer.';
-                break;
-            case 401:
-                this.statusText = 'Unauthorized, contact the administrator.';
-                break;
-            case 500:
-                this.statusText = 'Server error, contact the developer.';
-                break;
-            case 503:
-                this.statusText = 'Server busy, please wait a few seconds';
-                break;
-            default:
-                this.statusText = `Recieved unexpected status code: ${statusCode}
-                Contact the developer.`;
-        }
-        setTimeout(() => {
-            this.statusText = '';
-        }, 7000);
     }
 
     disableInvalidCards() {
         //Enforce that servers cant share ports
         // Get list of in-use ports
         let usedPorts: Set<number> = new Set();
-        this.iterateServerKeys((game, server) => {
-            if (this.serverData[game][server].is_online) {
-                usedPorts.add(this.serverData[game][server].port); // This port is in use
+        this.iterateServerKeys((data) => {
+            if (data.is_online) {
+                usedPorts.add(data.port); // This port is in use
             }
         });
 
-        this.iterateServerKeys((game, server) => {
+        this.iterateServerKeys((data) => {
             // If port is in use and the current server isn't the one using it
-            if (
-                usedPorts.has(this.serverData[game][server].port) &&
-                !this.serverData[game][server].is_online
-            ) {
-                this.serverData[game][server].queryDone = false;
+            if (usedPorts.has(data.port) && !data.is_online) {
+                data.queryDone = false;
             } else {
                 // Either port is not in use, or this *is* the server using the port
                 // Either way, allow buttons to be pressed
-                this.serverData[game][server].queryDone = true;
+                data.queryDone = true;
             }
         });
-    }
-
-    iterateServerKeys(fun: (game: string, server: string) => void) {
-        for (let game of Object.keys(this.serverData)) {
-            for (let server of Object.keys(this.serverData[game])) {
-                fun(game, server);
-            }
-        }
-    }
-
-    async fetchCardData() {
-        this.doneLoading = false;
-
-        let data = (await this.http
-            .get('/backend/servers-status', {
-                headers: {
-                    Authorization: `Bearer ${await this.loginService.firebase_auth.currentUser!.getIdToken()}`,
-                },
-            })
-            .toPromise()) as ServerData;
-
-        // This line should technically be the last thing to occur
-        // It has not yet caused problems though
-        this.serverData = data;
-
-        // This will add the missing keys that typescript thinks exists
-        this.disableInvalidCards();
-        //this.iterateServerKeys((game, server) => {
-        //    this.serverData[game][server].queryDone = false;
-        //});
     }
 
     async signInOut() {
@@ -183,4 +92,96 @@ export class ServerSelectComponent implements OnInit, OnDestroy {
             this.router.navigateByUrl('/servers/login');
         }
     }
+
+    async refresh() {
+        this.doneLoading = false;
+        this.serverData = await this.http.functions.fetchCardData();
+        this.doneLoading = true;
+    }
+
+    iterateServerKeys(fun: (data: ServerDataValue) => void) {
+        for (let game of Object.keys(this.serverData)) {
+            for (let server of Object.keys(this.serverData[game])) {
+                fun(this.serverData[game][server]);
+            }
+        }
+    }
+
+    http = {
+        functions: {
+            fetchCardData: async () => {
+                return (await this._httpClient
+                    .get('/backend/servers-status', {
+                        headers: {
+                            Authorization: `Bearer ${await this.loginService.firebase_auth.currentUser!.getIdToken()}`,
+                        },
+                    })
+                    .toPromise()) as ServerData;
+            },
+            sendCommand: async (data: QueryParams) => {
+                try {
+                    let res = await this._httpClient
+                        .post('/backend/server-command', null, {
+                            headers: {
+                                Authorization: `Bearer ${await this.loginService.firebase_auth.currentUser!.getIdToken()}`,
+                            },
+                            params: data,
+                            responseType: 'text',
+                            observe: 'response',
+                        })
+                        .toPromise();
+                    return res.status;
+                } catch (error) {
+                    if (error.status) {
+                        return error.status as number;
+                    } else {
+                        // There was an actual error
+                        throw error;
+                    }
+                }
+            },
+        },
+        handleServerResponse: (query: QueryParams, statusCode: number) => {
+            const { game, server, command } = query;
+            // Update what's online
+            switch (statusCode) {
+                case 200:
+                    switch (command) {
+                        case 'start':
+                        case 'restart':
+                            this.serverData[game][server].is_online = true;
+                            this.statusText = 'Command Successful';
+                            break;
+                        case 'stop':
+                            this.serverData[game][server].is_online = false;
+                            this.statusText = 'Command Successful';
+                            break;
+                        default:
+                            this.statusText =
+                                'Unimplemented Commandm contact the developer';
+                            break;
+                    }
+                    break;
+                case 400:
+                    this.statusText = 'Client Syntax, contact the developer.';
+                    break;
+                case 401:
+                    this.statusText =
+                        'Unauthorized, contact the administrator.';
+                    break;
+                case 500:
+                    this.statusText = 'Server error, contact the developer.';
+                    break;
+                case 503:
+                    this.statusText = 'Server busy, please wait a few seconds';
+                    break;
+                default:
+                    this.statusText = `Recieved unexpected status code: ${statusCode}
+                    Contact the developer.`;
+            }
+            setTimeout(() => {
+                this.statusText = '';
+            }, 7000);
+        },
+    };
 }
