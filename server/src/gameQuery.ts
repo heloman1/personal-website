08c3004781152e4d6f453185291d7aa614841b9a
Edward Gomez <2865652+heloman1@users.noreply.github.com>
2021-03-19 03:13:55 -0400

    basically split serverDetails in 2

---
+++ server/src/gameQuery.ts
@@ -0,0 +1,89 @@
+import child_process from "child_process";
+import { promisify } from "util";
+
+let shell = promisify(child_process.exec);
+
+interface ExpectedJSONData {
+    server: string;
+    details_string: string;
+}
+
+async function queryData(gameList: string[]): Promise<ExpectedJSONData[]> {
+    let jsonList: string[];
+    try {
+        let shell_output = await shell(
+            `ssh gameserver@edward-server './check_server_statuses.zsh '\\''${JSON.stringify(
+                gameList
+            )}'\\'''`
+        );
+        jsonList = shell_output.stdout.trim().split("\n");
+    } catch (err) {
+        console.error("fetchData: Error when executing shell command");
+        throw err;
+    }
+    let serverData: ExpectedJSONData[] = [];
+    for (let j of jsonList) {
+        try {
+            serverData.push(JSON.parse(j));
+        } catch (err) {
+            console.error("fetchData: error when parsing JSON");
+            console.error(`failing input: ${j}`);
+            console.error(err);
+        }
+    }
+    return serverData;
+}
+
+export interface ServerStatuses {
+    [game: string]: {
+        [server: string]: {
+            is_online: boolean;
+            port: number;
+        };
+    };
+}
+
+function formatData(jsonList: ExpectedJSONData[]): ServerStatuses {
+    let serverStatuses: ServerStatuses = {};
+    for (let j of jsonList) {
+        // server: "game/server"
+        // details_string: "Internet IP: 123.456.789.123:12345 Status: OFFLINE"
+        let [gameName, serverName] = j.server.split("/");
+        let [_0, _1, ip_port, _3, status] = j.details_string.split(" ");
+        let [_ip, port_string] = ip_port.split(":");
+
+        let port = Number.parseInt(port_string);
+        let is_online = status === "ONLINE";
+
+        if (!gameName) gameName = "Unknown";
+        if (!serverName) serverName = "Unknown";
+        if (!port) port = -1;
+
+        if (serverStatuses[gameName] === undefined) {
+            serverStatuses[gameName] = {};
+        }
+
+        serverStatuses[gameName][serverName] = {
+            is_online: is_online,
+            port: port,
+        };
+    }
+
+    return serverStatuses;
+}
+
+export default {
+    async fetchData(gameList: string[]) {
+        let jsonList = await queryData(gameList);
+        return formatData(jsonList);
+    },
+    async sendServerCommand(query: {
+        game: string;
+        server: string;
+        command: string;
+    }) {
+        return await shell(
+            `ssh gameserver@edward-server ./${query.game}/${query.server}/*server ${query.command}`
+        );
+    },
+};
--- server/src/routes/serverDetails.ts
+++ server/src/routes/serverDetails.ts
@@ -1,11 +1,10 @@
-import { Request, Router } from "express";
-import child_process from "child_process";
-import { promisify } from "util";
+import { Router } from "express";
 import config from "../config";
 import { decodeJWTToken } from "../firebase";
 import Condition from "../condition";
+import gameQuery from "../gameQuery";
+import { ServerStatuses } from "../gameQuery";
 
-let shell = promisify(child_process.exec);
 const conf = config.getConfig();
 const FIVE_MIN = 5 * 60 * 1000;
 let lastCheck = new Date(0).getTime();
@@ -15,87 +14,56 @@ router.use(decodeJWTToken);
 let currentlyChecking = new Condition(false);
 let executingCommand = false;
 
-let statusList: {
-    [game: string]: {
-        [server: string]: {
-            is_online: boolean;
-            port: number;
-        };
-    };
-} = {};
+let statusList: ServerStatuses = {};
 
 router.get("/servers-status", async (req, res) => {
     if (currentlyChecking.state === true) {
         await currentlyChecking.waitForCondition(false);
-    } else {
+    } else if (
+        req.query.force !== undefined ||
+        new Date().getTime() - lastCheck > FIVE_MIN
+    ) {
         currentlyChecking.state = true;
-        if (
-            req.query.force !== undefined ||
-            new Date().getTime() - lastCheck > FIVE_MIN
-        ) {
-            let jsonList: string[];
-            try {
-                let shell_output = await shell(
-                    `ssh gameserver@edward-server './check_server_statuses.zsh '\\''${JSON.stringify(
-                        conf.gameList
-                    )}'\\'''`
-                );
-                jsonList = shell_output.stdout.trim().split("\n");
-            } catch (err) {
-                res.sendStatus(500);
-                console.log(err);
-                return;
-            }
 
-            statusList = {};
-            jsonList.forEach((j) => {
-                let data: {
-                    server: string;
-                    details_string: string;
-                } = JSON.parse(j);
-                const [game, server] = data.server.split("/");
-                if (statusList[game] === undefined) {
-                    statusList[game] = {};
-                }
-                let [_0, _1, ip_port, _3, status] = data.details_string.split(
-                    " "
-                );
-                let [_ip, port] = ip_port.split(":");
-                statusList[game][server] = {
-                    is_online: status === "ONLINE",
-                    port: Number.parseInt(port),
-                };
-            });
+        try {
+            statusList = await gameQuery.fetchData(conf.gameList);
             lastCheck = new Date().getTime();
+        } catch (err) {
+            console.error("Error when fetching data");
+            console.error(err);
+            res.sendStatus(500);
+            return;
+        } finally {
+            currentlyChecking.state = false;
         }
-        currentlyChecking.state = false;
     }
 
     res.json(statusList);
 });
 
-async function sendServerCommand(query: any) {
-    await shell(
-        `ssh gameserver@edward-server ./${query.game}/${query.server}/*server ${query.command}`
-    );
-}
 router.post("/server-command", async (req, res) => {
-    if (executingCommand) {
-        res.sendStatus(503);
-    } else {
-        executingCommand = true;
-        try {
-            const { command, game, server } = req.query;
-            if (game && server && command) {
+    const { command, game, server } = req.query;
+    if (game && server && command) {
+        let query = {
+            game: game as string,
+            server: server as string,
+            command: command as string,
+        };
+
+        if (executingCommand) {
+            res.sendStatus(503); // Busy
+        } else {
+            executingCommand = true;
+            try {
                 let is_online;
                 switch (command) {
                     case "stop":
-                        await sendServerCommand(req.query);
+                        await gameQuery.sendServerCommand(query);
                         is_online = false;
                         break;
                     case "start":
                     case "restart":
-                        await sendServerCommand(req.query);
+                        await gameQuery.sendServerCommand(query);
                         is_online = true;
                         break;
                     default:
@@ -107,14 +75,15 @@ router.post("/server-command", async (req, res) => {
                     server as string
                 ].is_online = is_online;
                 res.sendStatus(200);
-            } else {
-                res.sendStatus(400);
+            } catch (err) {
+                console.log(err);
+                res.sendStatus(500);
+            } finally {
+                executingCommand = false;
             }
-        } catch (err) {
-            console.log(err);
-            res.sendStatus(500);
         }
-        executingCommand = false;
+    } else {
+        res.sendStatus(400);
     }
 });
 
