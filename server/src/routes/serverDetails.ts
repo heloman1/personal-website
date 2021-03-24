2fe8b1ca2480bcf0068475f8337ca23e51a23a5a
Edward Gomez <2865652+heloman1@users.noreply.github.com>
2021-03-24 17:20:18 -0400

    Typo, also logic error

--- server/src/routes/serverDetails.ts
+++ server/src/routes/serverDetails.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import Globals from "../globals";
-import { ServerStatuses } from "../globals";
 import { decodeJWTToken } from "../middleware/firebase";
 import gameQuery from "../data/gameQuery";
 import FunctionQueue from "../utils/functionQueue";
@@ -11,23 +10,23 @@ const FIVE_MIN = 5 * 60 * 1000;
 const router = Router();
 router.use(decodeJWTToken);
 
-let statusList: ServerStatuses = {};
-
 const queuedResponses = new FunctionQueue();
 let currentlyChecking = false;
 let lastCheck = new Date(0).getTime();
 router.get("/servers-status", async (req, res) => {
     if (currentlyChecking === true) {
+        // Webserver is currently querying, wait for it to finish before sending
         queuedResponses.addToQueue(() => {
-            res.json(statusList);
+            res.json(Globals.getGlobals().serverStatuses);
         });
-        return;
+        return; // Don't hit the res.json at the bottom
     }
 
     if (
         req.query.force !== undefined ||
         new Date().getTime() - lastCheck > FIVE_MIN
     ) {
+        // Time to update data
         currentlyChecking = true;
 
         try {
@@ -36,16 +35,15 @@ router.get("/servers-status", async (req, res) => {
             );
             lastCheck = new Date().getTime();
         } catch (err) {
-            console.error("Error when fetching data");
             console.error(err);
             res.sendStatus(500);
             return;
         } finally {
             currentlyChecking = false;
         }
-        res.json(statusList);
-        queuedResponses.runAll();
     }
+    res.json(Globals.getGlobals().serverStatuses);
+    queuedResponses.consumeAll(); // This will do nothing if its empty
 });
 
 export default router;
