117686d5a5aaee9623ded09b1e030c89dfbefdc9
Edward Gomez <2865652+heloman1@users.noreply.github.com>
2021-09-03 15:00:07 -0400

    using systemd instead of tmux now

--- server/start.sh
+++ server/start.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env sh
 cd "${0%/*}"
 
-TMUX_SESSION=webserver
-echo 'Killing Web Server'
-tmux kill-session -t $TMUX_SESSION
+# TMUX_SESSION=webserver
+# echo 'Killing Web Server'
+# tmux kill-session -t $TMUX_SESSION
 
 #echo 'Copying Certs'
 #sudo ln -s /etc/letsencrypt/live/edwardgomez.dev/privkey.pem privkey.pem
 #sudo ln -s /etc/letsencrypt/live/edwardgomez.dev/fullchain.pem fullchain.pem
 #sudo chown $USER:$USER creds/privkey.pem creds/fullchain.pem
 
+set -e
+echo "Sourcing nvm (and npm, and node)"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use
 echo 'Starting Web Server'
-
-tmux new -d -s $TMUX_SESSION "GOOGLE_APPLICATION_CREDENTIALS='./creds/firebase.json' node build/index.js data/config.json"
+npm start
