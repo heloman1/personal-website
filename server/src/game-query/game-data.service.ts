4e845aca6d20f9b59ebac5dcdb4f4c91fd7051e1
Edward Gomez <2865652+heloman1@users.noreply.github.com>
2021-09-28 13:37:24 -0400

    Get folder list from config

--- server/src/game-query/game-data.service.ts
+++ server/src/game-query/game-data.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { OutgoingServerStatuses } from 'src/types';
 import { BehaviorSubject, firstValueFrom } from 'rxjs';
 import { GameQueryService } from './game-query.service';
+import { ConfigService } from 'src/config/config.service';
 
 const FIVE_MIN = 5 * 60 * 1000;
 let lastCheck = new Date(0).getTime();
@@ -11,7 +12,10 @@ let lastCheck = new Date(0).getTime();
  * Handles the Server Data
  */
 export class GameDataService {
-  constructor(private readonly query: GameQueryService) {}
+  constructor(
+    private readonly query: GameQueryService,
+    private readonly config: ConfigService,
+  ) {}
   private get isDataFresh() {
     // If its been 5 minutes, the data is no longer fresh
     // (the exact time is arbtrary)
@@ -28,7 +32,7 @@ export class GameDataService {
     } else {
       this.query.isQueryingOrCommanding = true;
       // Grab the data, emit the data, mark the data as fresh
-      const out = await this.query.fetchData(['yup']);
+      const out = await this.query.fetchData(this.config.folderList);
       this.data.next(out);
       lastCheck = Date.now();
       this.query.isQueryingOrCommanding = false;
