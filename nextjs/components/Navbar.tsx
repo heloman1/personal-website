48943f6b08fa046201c8399c687c8ad81d196a27
Edward Gomez <2865652+heloman1@users.noreply.github.com>
2021-12-18 02:03:34 -0500

    responsive navbar

--- nextjs/components/Navbar.tsx
+++ nextjs/components/Navbar.tsx
@@ -2,8 +2,15 @@ import Button from "@mui/material/Button";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Link from "next/link";
-import { PropsWithChildren, useState } from "react";
-import { ButtonGroup, Grid, IconButton } from "@mui/material";
+import { PropsWithChildren, useEffect, useState } from "react";
+import {
+    ButtonGroup,
+    Drawer,
+    Grid,
+    IconButton,
+    List,
+    ListItem,
+} from "@mui/material";
 import {
     Computer,
     ComputerTwoTone,
@@ -11,6 +18,7 @@ import {
     DarkModeOutlined,
     LightMode,
     LightModeOutlined,
+    Menu,
 } from "@mui/icons-material";
 import { ColorTheme } from "../additional";
 
@@ -20,37 +28,91 @@ type NavbarProps = PropsWithChildren<{
 }>;
 
 export default function Navbar({ theme, setTheme, children }: NavbarProps) {
+    const [state, setState] = useState({
+        mobile: false,
+        drawerOpen: false,
+    });
+    const openDrawer = () => {
+        setState({ ...state, drawerOpen: true });
+    };
+    const closeDrawer = () => {
+        setState({ ...state, drawerOpen: false });
+    };
+
+    const links = [
+        ["Home", "/"],
+        ["Server Control", "/server-control"],
+        ["About", "/about"],
+    ];
+    useEffect(() => {
+        const checkWidth = () => {
+            window.innerWidth < 700
+                ? setState((s) => {
+                      return { ...s, mobile: true };
+                  })
+                : setState((s) => {
+                      return { ...s, mobile: false };
+                  });
+        };
+
+        checkWidth();
+        window.addEventListener("resize", checkWidth);
+
+        return () => window.removeEventListener("resize", checkWidth);
+    }, []);
+
     return (
         <AppBar position="sticky">
             <Toolbar>
                 <Grid container>
-                    <Grid container item xs={6} spacing={2}>
-                        <Grid item>
-                            <Button color="inherit">
-                                <Link href="/">Home</Link>
-                            </Button>
-                        </Grid>
-
-                        <Grid item>
-                            <Button color="inherit">
-                                <Link href="/server-control">
-                                    Server Control
-                                </Link>
-                            </Button>
-                        </Grid>
-
-                        <Grid item>
-                            <Button color="inherit">
-                                <Link href="/about">About</Link>
-                            </Button>
-                        </Grid>
+                    <Grid container item xs={state.mobile ? 1 : 6} spacing={2}>
+                        {state.mobile ? (
+                            <>
+                                <Drawer
+                                    anchor="top"
+                                    open={state.drawerOpen}
+                                    onClose={closeDrawer}
+                                >
+                                    <List>
+                                        {links.map(([name, link], i) => (
+                                            <ListItem key={i}>
+                                                <Button
+                                                    onClick={closeDrawer}
+                                                    color="inherit"
+                                                >
+                                                    <Link href={link}>
+                                                        {name}
+                                                    </Link>
+                                                </Button>
+                                            </ListItem>
+                                        ))}
+                                    </List>
+                                </Drawer>
+                                <Grid item>
+                                    <IconButton onClick={openDrawer}>
+                                        <Menu />
+                                    </IconButton>
+                                </Grid>
+                            </>
+                        ) : (
+                            <>
+                                {links.map(([name, link], i) => (
+                                    <Grid item key={i}>
+                                        <Button color="inherit">
+                                            <Link href={link}>{name}</Link>
+                                        </Button>
+                                    </Grid>
+                                ))}
+                            </>
+                        )}
                     </Grid>
 
                     <Grid
                         container
                         item
-                        xs={6}
-                        justifyContent="end"
+                        xs={state.mobile ? 11 : 6}
+                        justifyContent="flex-end"
+                        alignContent="center"
                         spacing={2}
                     >
                         {/* Extra Buttons */}
