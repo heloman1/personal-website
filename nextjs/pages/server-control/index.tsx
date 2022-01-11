2a4e85aee10b6d41c6ea52551e54ad7d2b111872
Edward Gomez <2865652+heloman1@users.noreply.github.com>
2022-01-10 19:11:28 -0500

    Turned class back into a FunctionComponent

--- nextjs/pages/server-control/index.tsx
+++ nextjs/pages/server-control/index.tsx
@@ -11,8 +11,12 @@ import {
 } from "@mui/material";
 import styles from "../../styles/ServerControl.module.css";
 import Link from "next/link";
-import { Component } from "react";
-import type { ColorTheme, ServerStatusesWithDisabled } from "../../additional";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import type {
+    ColorTheme,
+    NextPageWithNavbarOverride,
+    ServerStatusesWithDisabled,
+} from "../../additional";
 import { GameServerCard } from "../../components/GameServerCard";
 import Navbar from "../../components/Navbar";
 
@@ -59,48 +63,53 @@ type ServerControlState = {
     snackbarSeverity: AlertColor;
 };
 
-export default class ServerControl extends Component<
-    ServerControlProps,
-    ServerControlState
-> {
-    // So _app.tsx doesnt create a navbar
-    static readonly isOverridingNavbar: Readonly<boolean> = true;
-    state: Readonly<ServerControlState> = {
-        serverData: {},
+const ServerControl: NextPageWithNavbarOverride<ServerControlProps> = (
+    props
+) => {
+    const [state, setState] = useState<ServerControlState>({
         loading: false,
+        serverData: {},
         snackbarIsOpen: false,
-        snackbarMessage: "Yep",
+        snackbarMessage: "",
         snackbarSeverity: "info",
-    };
+    });
+    const fetchAborter = useMemo(() => new AbortController(), []);
 
-    constructor(props: ServerControlProps) {
-        super(props);
-        this.refreshData = this.refreshData.bind(this);
-        this.doButtonAction = this.doButtonAction.bind(this);
-        this.showSnackbar = this.showSnackbar.bind(this);
-        this.hideSnackbar = this.hideSnackbar.bind(this);
-    }
-    doButtonAction(
+    const showSnackbar = useCallback(
+        (message: string, severity?: AlertColor) => {
+            setState((state) => {
+                return {
+                    ...state,
+                    snackbarIsOpen: true,
+                    snackbarMessage: message,
+                    snackbarSeverity: severity || "info",
+                };
+            });
+        },
+        [setState]
+    );
+    const hideSnackbar = useCallback(() => {
+        setState((s) => {
+            return { ...s, snackbarIsOpen: false };
+        });
+    }, [setState]);
+
+    // Don't use useCallback here, it skips a rerender if you do that
+    const sendCommand = async (
         game: string,
         server: string,
         action: "start" | "stop" | "restart"
-    ): void {
-        this.sendCommand(game, server, action);
-    }
-
-    async sendCommand(
-        game: string,
-        server: string,
-        action: "start" | "stop" | "restart"
-    ) {
-        this.setState({ loading: true });
+    ) => {
+        setState((s) => {
+            return { ...s, loading: true };
+        });
         let res: Response;
         try {
             res = await fetch(
                 `api/server-control/servers/${game}/${server}/${action}`,
                 {
                     method: "POST",
-                    signal: this.fetchAborter.signal,
+                    signal: fetchAborter.signal,
                 }
             );
         } catch (e) {
@@ -112,12 +121,15 @@ export default class ServerControl extends Component<
         switch (res.status) {
             case 200:
                 const serverData: ServerStatusesWithDisabled = await res.json();
+
                 // serverData doesn't actually have disabled keys on it yet
                 // This is done so Typescript doesn't get in the way
                 // When enforceUsedPorts coincidentally adds them
                 enforceUsedPorts(serverData);
-                this.setState({ serverData, loading: false });
-                this.showSnackbar("Command Successful", "success");
+                setState((s) => {
+                    return { ...s, serverData, loading: false };
+                });
+                showSnackbar("Command Successful", "success");
                 return;
             case 405:
                 errorText = `sendCommand sent a POST request and recieved ${res.status} - ${res.statusText}`;
@@ -133,19 +145,24 @@ export default class ServerControl extends Component<
                 errorText = `Unexpected error ${res.status} - ${res.statusText}`;
                 break;
         }
-        this.showSnackbar(errorText, "error");
-        this.setState({ loading: false });
+        showSnackbar(errorText, "error");
+        setState((s) => {
+            return { ...s, loading: false };
+        });
         throw errorText;
-    }
-    async refreshData() {
+    };
+
+    const refreshData = useCallback(async () => {
         // serverData doesn't actually have disabled keys on it yet
         // This is done so Typescript doesn't get in the way...
-        this.setState({ loading: true });
+        setState((s) => {
+            return { ...s, loading: true };
+        });
         // : ServerStatusesWithDisabled
         let res: Response;
         try {
             res = await fetch("/api/server-control/servers", {
-                signal: this.fetchAborter.signal,
+                signal: fetchAborter.signal,
             });
         } catch (e) {
             console.log("Fetch was aborted. Did you navigate away?");
@@ -156,11 +173,17 @@ export default class ServerControl extends Component<
         switch (res.status) {
             case 200:
                 const serverData: ServerStatusesWithDisabled = await res.json();
+
                 // serverData doesn't actually have disabled keys on it yet
                 // This is done so Typescript doesn't get in the way
                 // When enforceUsedPorts coincidentally adds them
                 enforceUsedPorts(serverData);
-                this.setState({ serverData, loading: false });
+                console.log("Enforcing data");
+
+                setState((s) => {
+                    return { ...s, serverData, loading: false };
+                });
+                console.log("Data updated");
                 return;
             case 405:
                 errorText = `refreshData sent a GET request and recieved ${res.status} - ${res.statusText}`;
@@ -172,136 +195,118 @@ export default class ServerControl extends Component<
                 errorText = `Unexpected error ${res.status} - ${res.statusText}`;
                 break;
         }
-        this.setState({ loading: false });
-        throw errorText;
-    }
-
-    // Will abort on unmount
-    private fetchAborter = new AbortController();
-    componentDidMount() {
-        // On mount, automatically grab data
-        this.refreshData();
-    }
-
-    componentWillUnmount() {
-        try {
-            this.fetchAborter.abort();
-        } catch (e) {
-            // console.log("Fetch Aborted");
-        }
-    }
-
-    showSnackbar(message: string, severity?: AlertColor) {
-        this.setState({
-            snackbarIsOpen: true,
-            snackbarMessage: message,
-            snackbarSeverity: severity || "info",
+        setState((s) => {
+            return { ...s, loading: false };
         });
-    }
+        throw errorText;
+    }, [fetchAborter.signal, setState]);
 
-    hideSnackbar() {
-        this.setState({ snackbarIsOpen: false });
-    }
-
-    render() {
-        return (
-            <>
-                <Navbar theme={this.props.theme} setTheme={this.props.setTheme}>
-                    <ButtonGroup>
-                        {/* Refresh Button */}
-                        <Button
-                            disabled={this.state.loading}
-                            onClick={this.refreshData}
-                            color="inherit"
-                            variant="outlined"
-                        >
-                            <Cached
-                                className={
-                                    this.state.loading ? styles.spin : ""
-                                }
-                            />
-                        </Button>
-
-                        <Button color="inherit" variant="outlined">
-                            <Link href="/server-control/login">Login</Link>
-                        </Button>
-                    </ButtonGroup>
-                </Navbar>
-                <main>
-                    <Snackbar
-                        autoHideDuration={5000}
-                        anchorOrigin={{
-                            vertical: "bottom",
-                            horizontal: "center",
-                        }}
-                        open={this.state.snackbarIsOpen}
-                        onClose={(_e, r) => {
-                            if (r == "timeout") {
-                                this.hideSnackbar();
-                            }
-                        }}
+    useEffect(() => {
+        refreshData();
+        // vvv Because onMount only vvv
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    useEffect(
+        () => () => {
+            try {
+                fetchAborter.abort();
+            } catch (e) {
+                // console.log("Fetch Aborted");
+            }
+        },
+        [fetchAborter]
+    );
+    return (
+        <>
+            <Navbar theme={props.theme} setTheme={props.setTheme}>
+                <ButtonGroup>
+                    {/* Refresh Button */}
+                    <Button
+                        disabled={state.loading}
+                        onClick={refreshData}
+                        color="inherit"
+                        variant="outlined"
                     >
-                        <Alert
-                            sx={{ alignItems: "center" }}
-                            severity={this.state.snackbarSeverity}
-                            action={
-                                <IconButton
-                                    color="inherit"
-                                    onClick={this.hideSnackbar}
-                                >
-                                    <Close color="inherit" />
-                                </IconButton>
-                            }
-                        >
-                            {this.state.snackbarMessage}
-                        </Alert>
-                    </Snackbar>
-                    {/* Server Data Display Grid */}
-                    <Grid container paddingLeft=".5rem" paddingRight=".5rem">
-                        {Object.keys(this.state.serverData).map((game, id) => (
-                            <Grid container item key={id} spacing={2}>
-                                {/* Title (uses 12 to take up entire line) */}
-                                <Grid item xs={12} marginTop="1rem">
-                                    <Typography variant="h4">{game}</Typography>
-                                </Grid>
-                                {Object.keys(this.state.serverData[game]).map(
-                                    (server, id) => {
-                                        const { is_online, port, disabled } =
-                                            this.state.serverData[game][server];
-                                        return (
-                                            // Server Card
-                                            <Grid
-                                                item
-                                                key={id}
-                                                xs={12}
-                                                sm={6}
-                                                md={3}
-                                                lg={2}
-                                            >
-                                                <GameServerCard
-                                                    setServerData={
-                                                        this.doButtonAction
-                                                    }
-                                                    serverProps={{
-                                                        disabled:
-                                                            this.state
-                                                                .loading ||
-                                                            disabled,
-                                                        game,
-                                                        server,
-                                                        is_online,
-                                                        port,
-                                                    }}
-                                                />
-                                            </Grid>
-                                        );
-                                    }
-                                )}
+                        <Cached className={state.loading ? styles.spin : ""} />
+                    </Button>
+
+                    <Button color="inherit" variant="outlined">
+                        <Link href="/server-control/login">Login</Link>
+                    </Button>
+                </ButtonGroup>
+            </Navbar>
+            <main>
+                <Snackbar
+                    autoHideDuration={5000}
+                    anchorOrigin={{
+                        vertical: "bottom",
+                        horizontal: "center",
+                    }}
+                    open={state.snackbarIsOpen}
+                    onClose={(_e, r) => {
+                        if (r == "timeout") {
+                            hideSnackbar();
+                        }
+                    }}
+                >
+                    <Alert
+                        sx={{ alignItems: "center" }}
+                        severity={state.snackbarSeverity}
+                        action={
+                            <IconButton color="inherit" onClick={hideSnackbar}>
+                                <Close color="inherit" />
+                            </IconButton>
+                        }
+                    >
+                        {state.snackbarMessage}
+                    </Alert>
+                </Snackbar>
+                {/* Server Data Display Grid */}
+                <Grid container paddingLeft=".5rem" paddingRight=".5rem">
+                    {Object.keys(state.serverData).map((game, id) => (
+                        <Grid container item key={id} spacing={2}>
+                            {/* Title (uses 12 to take up entire line) */}
+                            <Grid item xs={12} marginTop="1rem">
+                                <Typography variant="h4">{game}</Typography>
                             </Grid>
-                        ))}
-                    </Grid>
-                </main>
-            </>
-        );
-    }
-}
+                            {Object.keys(state.serverData[game]).map(
+                                (server, id) => {
+                                    const { is_online, port, disabled } =
+                                        state.serverData[game][server];
+                                    return (
+                                        // Server Card
+                                        <Grid
+                                            item
+                                            key={id}
+                                            xs={12}
+                                            sm={6}
+                                            md={3}
+                                            lg={2}
+                                        >
+                                            <GameServerCard
+                                                setServerData={sendCommand}
+                                                serverProps={{
+                                                    disabled:
+                                                        state.loading ||
+                                                        disabled,
+                                                    game,
+                                                    server,
+                                                    is_online,
+                                                    port,
+                                                }}
+                                            />
+                                        </Grid>
+                                    );
+                                }
+                            )}
+                        </Grid>
+                    ))}
+                </Grid>
+            </main>
+        </>
+    );
+};
+
+ServerControl.isOverridingNavbar = true;
+
+export default ServerControl;
