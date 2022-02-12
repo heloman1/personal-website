b50feaff86adc8b092ce1f3ffa6fab239763fb64
Edward Gomez <2865652+heloman1@users.noreply.github.com>
2022-02-11 21:44:10 -0500

    migrate to nextjs

--- client/src/app/bad-route/bad-route.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-bad-route',
-    templateUrl: './bad-route.component.html',
-    styleUrls: ['./bad-route.component.scss'],
-})
-export class BadRouteComponent {}
